13a1b71e63c5371bc8a445d02821429c5a024bc0
Parinya <parinya@igeargeek.com>
2019-06-30 16:44:01 +0700

    [Edited] styling homepage with bootstrap and raw css

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use('/css', express.static(__dirname + '/css'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(process.env.port || 3000)
+app.listen(process.env.port || 3000, () => {
+  console.log('running on port 3000')
+})
