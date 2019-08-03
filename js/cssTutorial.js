b2008a8ff496322f48fb41db5c194b87e09590ee
Parinya <parinya@igeargeek.com>
2019-08-03 20:06:44 +0700

    [Edited] set initial css element

--- js/cssTutorial.js
+++ js/cssTutorial.js
@@ -1,12 +1,16 @@
 const codeEditor = () => {
   const css = document.getElementById('css')
   const code = document.getElementById('code').contentWindow.document
+  const elements = '<h1>&lt;h1&gt;Red Text&lt;/h1&gt;</h1>' +
+      '<h2 id="green">&lt;h2 id="green"&gt;Green Border&lt;/h2&gt;</h2>' +
+      '<h3 class="blue">&lt;h3 class="blue"&gt;Blue Background&lt;/h3&gt;</h3>'
+  code.open()
+  code.writeln(elements)
+  code.close()
+
   document.body.onkeyup = () => {
     code.open()
-    code.writeln('<h1>&lt;h1&gt;Red Text&lt;/h1&gt;</h1>')
-    code.writeln('<h2 id="green">&lt;h2 id="green"&gt;Green Border&lt;/h2&gt;</h2>')
-    code.writeln('<h3 class="blue">&lt;h3 class="blue"&gt;Blue Background&lt;/h3&gt;</h3>')
-    code.writeln(`<style>${css.value}</style>`)
+    code.writeln(`${elements}<style>${css.value}</style>`)
     code.close()
   }
 }
